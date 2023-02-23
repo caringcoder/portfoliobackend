25518822fe9c3bb3667ffa95bc06d661f212f166
Aditya Raj Chauhan <102533268+caringcoder@users.noreply.github.com>
2023-02-24 00:04:12 +0530

    fixes

--- routes/visitor.js
+++ routes/visitor.js
@@ -4,39 +4,16 @@ const ip = require('ip')
 
 const router = express.Router();
 
-// router.get('/', async (req, res) => {
-//     const visitor = await Visitor.findOne({ name: "TotalVisitors" })
-//     if (visitor == null) {
-//      const beginCount = new Visitor({
-//         name : "TotalVisitior",
-//         visitorCount : 1
-//      })
-//      beginCount.save()
-
-//      // Sending the count of visitor to the browser
-//      res.send(`<h2>Counter: `+1+'</h2>')
-//     }
-//     else {
-//         visitor.visitorCount += 1;
-//         // Saving to the database
-//         visitor.save()
-
-//         // Sending the count of visitor to the browser
-//         res.send(`<h2>Counter: `+visitor.visitorCount+'</h2>')
-//     }
-// })
-
 router.get('/', async (req, res) => {
-
+    let visitorList = await Visitor.find()
     let currentIp = ip.address();
-    let visitors =  await Visitor.count('visitorIp');
-    console.log(visitors-1)
-        const uniqueVisit = new Visitor({
-            visitorIp: currentIp
-        })
-        const saving = await uniqueVisit.save();
-        res.json(saving)
-    
+
+    const uniqueVisit = new Visitor({
+        visitorIp: currentIp
+    })
+    const saving = await uniqueVisit.save();
+    res.json(visitorList)
+
 })
 
 module.exports = router;
